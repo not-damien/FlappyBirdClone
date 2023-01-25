024ea59ac9cf0727b7130d8b6e5eabb8d413ba4c
not-damien <93546644+not-damien@users.noreply.github.com>
2023-01-24 19:28:21 -0800

    added pipes

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./pochita pixel boi.png")
+ASSET_MANAGER.queueDownload("./pipe.png")
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
 	gameEngine.init(ctx);
 	gameEngine.addEntity(new player(gameEngine,10,10))
+	gameEngine.addEntity(new pipe(gameEngine,50,50))
 	gameEngine.start();
 });
---
+++ pipe.js
@@ -0,0 +1,22 @@
+class pipe {
+    constructor(game, x, y) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.spritesheet = ASSET_MANAGER.getAsset("./pipe.png");
+        this.BoundingBox = new BoundingBox(this.x, this.y, 50, 100);
+    };
+    update() {
+        
+    };
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, this.x, this.y, 50, 100);
+        this.BoundingBox.draw(ctx);
+    };
+    updateBox() {
+        this.BoundingBox = new BoundingBox(this.x, this.y, 50, 100);
+    }
+
+
+
+}
