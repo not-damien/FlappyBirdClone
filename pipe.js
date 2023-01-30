d466e8c9ab283a6a8a20b52b3130f412e4acf613
not-damien <93546644+not-damien@users.noreply.github.com>
2023-01-29 17:53:04 -0800

    animated chainsaws

--- pipe.js
+++ pipe.js
@@ -3,14 +3,17 @@ class pipe {
         this.game = game;
         this.x = x;
         this.y = y;
-        this.spritesheet = ASSET_MANAGER.getAsset("./chainPipe.png");
-        this.BoundingBox = new BoundingBox(this.x, this.y, 50, 100);
+        this.spritesheet = ASSET_MANAGER.getAsset("./chainsawAni.png");
+        this.BoundingBox = new BoundingBox(this.x, this.y, 50, 500);
+        this.animation =  new Animator(this.spritesheet, 0, 0, 50, 500, 2, .1, 0, false, true);
+                                                    //spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop
     };
     update() {
 
     };
     draw(ctx) {
-        ctx.drawImage(this.spritesheet, this.x, this.y, 50, 500);
+        //ctx.drawImage(this.spritesheet, this.x, this.y, 50, 500);
+        this.animation.drawFrame(this.game.clockTick,ctx,this.x,this.y,1);
        // this.BoundingBox.draw(ctx);
         
     };
